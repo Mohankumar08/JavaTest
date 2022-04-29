d5e72bf243c54034057f97dc973e6164fab1badb
Mohankumar08 <pmoankumar4@gmail.com>
2022-04-29 16:08:27 +0530

    B works

--- src/org/basic/OddorEven.java
+++ src/org/basic/OddorEven.java
@@ -1,6 +1,7 @@
 package org.basic;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class OddorEven {
 
@@ -14,6 +15,29 @@ public class OddorEven {
 		}
 		System.out.println("Factorial of given no is: " + count);
 
-	}
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		String a = "java";
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		
+		String original = sc.nextLine();
+		Pattern pattern = Pattern.compile("\\s");
+		String[] temp = pattern.split(original);
+	     
+		String reverse = "";
+		
+		int length = original.length();
+		 for (int i = 0; i < temp.length; i++) {
+	            if (i == temp.length - 1)
+	                reverse = temp[i] + reverse;
+	            else
+	                reverse = " " + temp[i] + reverse;
+	        }
+	  
+		System.out.println(reverse);
+	
+}
 }
 
